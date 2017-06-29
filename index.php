ff009d9ad9277b87f10ddde129ef30289292043f
Swathi Krishnan <krishnanswathi18@gmail.com>
2017-06-29 13:13:54 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Poojappura Central Jail';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>POOJAPPURA CENTRAL JAIL</h1>
+<img src="image9.jpg"  style="width:1000px;height:500px;">
+<div class="container-fluid">
+
+
+<style>
+body{
+ 
+background-color:#eeeeee;
+}
+</style>`
+</div>
+
+        <!--<p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>-->
+
+<!--<style>
+body{
+ background-image:url("image9.jpg");
+}
+</style>--> 
+    </div>
+<!--<div class="container bimg"></div>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<!--<h2 class="text-center">test1</h2>
+			<p class="lead text-center">test2</p>
+			<p class="text-center"><a class="btn btn-lg btn-success"
+						  href="http://localhost/"><span
+				class="glyphicon glyphicon-plus"></span>something</a></p>
+</div>
+</div>
+</div> -->
+
+   <div class="body-content">
+
+        <div class="row">
+          <!--  <div class="col-lg-4">
+                <h2>Heading</h2> -->
+
+              <p>Central Prison, Poojappura, is situated in Poojappura in Thiruvananthapuram Kerala state of India. It was established in Kerala in the year of 1914 as a part of Travancore [1] It is one of the three central prisons other than Kannur and Viyyur, situated in Kerala.</p>
+
+
+
+              <!--  <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p> -->
+            </div>
+
+       
+           <!-- <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Heading</h2>
+
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                    fugiat nulla pariatur.</p>
+
+                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            </div>-->
+        </div>
+
+    </div>
+</div>
